04b4fc8b95554d6a7ddaeb91695c1780bd2f0038
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-11-18 11:51:27 +0100

    Generate project with start.spring.io

---
+++ src/test/java/com/example/webfluxjwt/WebfluxJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
