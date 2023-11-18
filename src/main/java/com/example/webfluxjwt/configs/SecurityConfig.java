34190d425809f9039dc61102a073c2ccf8777408
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-11-18 12:32:46 +0100

    Fix security config

--- src/main/java/com/example/webfluxjwt/configs/SecurityConfig.java
+++ src/main/java/com/example/webfluxjwt/configs/SecurityConfig.java
@@ -1,16 +1,24 @@
 package com.example.webfluxjwt.configs;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public MapReactiveUserDetailsService userDetailsService(PasswordEncoder encoder) {
